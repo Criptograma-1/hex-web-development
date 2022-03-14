8551a954a1802e2b1054895b94abf3ddc3ad0dca
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-03-14 16:05:00 -0300

    Add documentation

--- 0x0A-caching/0-basic_cache.py
+++ 0x0A-caching/0-basic_cache.py
@@ -12,13 +12,16 @@ class BasicCache(BaseCaching):
        -This caching system doesn’t have limit
     """
     def __init__(self) -> None:
+        """BaseCaching Caching System"""
         super().__init__()
 
     def put(self, key, item):
+        """Put function definition"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Get function definition"""
         if key is not None:
             for k, value in self.cache_data.items():
                 if k == key:
