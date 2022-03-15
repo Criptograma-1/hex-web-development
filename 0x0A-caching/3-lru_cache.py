09cf5a3c6549b04702f1136898536c268a9e3416
Criptograma-1 <andresa.info@gmail.com>
2022-03-15 14:19:08 +0000

    Add task 3

---
+++ 0x0A-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Basic dictionary LRUCache Cache
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache Caching System"""
+
+    def __init__(self) -> None:
+        """Init function"""
+        super().__init__()
+        self.list_name = []
+
+    def put(self, key, item):
+        """Put function"""
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.list_name:
+                self.list_name.append(key)
+            else:
+                if self.list_name[-1] != key:
+                    self.list_name.remove(key)
+                    self.list_name.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.list_name[0]))
+                del(self.cache_data[self.list_name[0]])
+                self.list_name.pop(0)
+
+    def get(self, key):
+        """Get function"""
+        if key is not None:
+            if key in self.list_name:
+                if self.list_name[-1] != key:
+                    self.list_name.remove(key)
+                    self.list_name.append(key)
+             return self.cache_data[key]
+        return None
