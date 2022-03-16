2be8ce17e015713090f456a2b388fd694edd122e
Criptograma-1 <andresa.info@gmail.com>
2022-03-16 13:28:06 +0000

    Fix Pycodestyle

--- 0x0C-personal_data/filtered_logger.py
+++ 0x0C-personal_data/filtered_logger.py
@@ -12,10 +12,11 @@ def filter_datum(fields: List[str],
     """Function that use a regex to replace occurrences of certain field values
        Args:
             -fields: a list of strings representing all fields to obfuscate.
-            -redaction: a string representing by what the field will be obfuscated
+            -redaction: a string representing by what the field will
+                        be obfuscated
             -message: a string representing the log line
-            -separator: a string representing by which character is separating all
-                        fields in the log line (message).
+            -separator: a string representing by which character is
+                        separating all fields in the log line (message).
        Return:
             -the log message obfuscated
     """
