f1697ac9c72a9a6b99c79704680ffa552e2e5b98
Criptograma-1 <andresa.info@gmail.com>
2022-07-12 17:26:58 +0000

    Add task 4. Define which routes don't need authentication

--- 0x0D-Basic_authentication/api/v1/auth/auth.py
+++ 0x0D-Basic_authentication/api/v1/auth/auth.py
@@ -13,9 +13,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        returns False - path and excluded_paths
+        Return: 
+            - True if the path is not in the list of strings excluded_paths
+            - False if the path is in the list of strings excluded_paths
         """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        
+        if path in excluded_paths or f"{path}/" in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
