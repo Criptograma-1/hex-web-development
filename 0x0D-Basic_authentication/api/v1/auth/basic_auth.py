2467a0a26383c339adde56d149b06e692b4c85d6
Criptograma-1 <andresa.info@gmail.com>
2022-07-12 18:48:34 +0000

    [ADD] 7. Basic - Base64 part

--- 0x0D-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x0D-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,3 +14,20 @@ class BasicAuth(Auth):
         Initialize the class
         """
         super().__init__()
+
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        elif type(authorization_header) is not str:
+            return None
+        elif authorization_header[:6] != "Basic ":
+            return None
+        else:
+            return authorization_header.split(" ")[1]
