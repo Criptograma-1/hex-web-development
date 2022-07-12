09bafd7fd00cedc897ecef39fbe03ad7b50b162a
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-12 15:30:45 -0300

    Update app.py

--- 0x0D-Basic_authentication/api/v1/app.py
+++ 0x0D-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
