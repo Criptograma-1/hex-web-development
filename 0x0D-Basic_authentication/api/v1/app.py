fde6044d413d1e42c3a7500041abc871770c5d62
Criptograma-1 <andresa.info@gmail.com>
2022-07-12 15:41:53 +0000

    Add task 1. Error handler: Unauthorized

--- 0x0D-Basic_authentication/api/v1/app.py
+++ 0x0D-Basic_authentication/api/v1/app.py
@@ -20,6 +20,12 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Error handler: Unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- 0x0D-Basic_authentication/api/v1/views/index.py
+++ 0x0D-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,7 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    abort(401)
