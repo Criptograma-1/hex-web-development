1e3b293b2a279b21d12c3af254ff987af9b5f928
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-12 12:47:43 -0300

    Fix pycodestyle
    Fix pycodestyle validation: api/v1/app.py

--- 0x0D-Basic_authentication/api/v1/app.py
+++ 0x0D-Basic_authentication/api/v1/app.py
@@ -20,6 +20,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Error handler: Unauthorized
