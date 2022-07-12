35a6e4fa63ee76f208f57c2405880fd6a02c3a51
Criptograma-1 <andresa.info@gmail.com>
2022-07-12 17:07:35 +0000

    Add task 3. Auth class

---
+++ 0x0D-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    A class to manage the API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns False - path and excluded_paths
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns None - request
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None - request
+        """
+        return None
+
