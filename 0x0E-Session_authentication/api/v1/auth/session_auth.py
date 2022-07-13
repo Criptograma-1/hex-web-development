977a29947d878b6dd9b894e469759943371c72e7
Criptograma-1 <andresa.info@gmail.com>
2022-07-13 14:50:52 +0000

    [ADD] 3. User ID for Session ID

--- 0x0E-Session_authentication/api/v1/auth/session_auth.py
+++ 0x0E-Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,9 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a User ID based on a Session ID"""
+         if session_id is None or type(session_id) is not str:
+             return None
+         return self.user_id_by_session_id.get(session_id)
