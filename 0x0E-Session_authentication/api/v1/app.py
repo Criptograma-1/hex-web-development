858cfa265821d9f77f5b170884800f24141242ca
Criptograma-1 <andresa.info@gmail.com>
2022-07-13 15:11:53 +0000

    [ADD] 5. Before request

--- 0x0E-Session_authentication/api/v1/app.py
+++ 0x0E-Session_authentication/api/v1/app.py
@@ -57,9 +57,10 @@ def before_request():
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/']
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/']
     if auth and auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
