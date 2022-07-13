aadc91a207a3f5fb30b1251fa64d665344f39216
Criptograma-1 <andresa.info@gmail.com>
2022-07-13 14:53:37 +0000

    [FIX] Identation Error

--- 0x0E-Session_authentication/api/v1/auth/session_auth.py
+++ 0x0E-Session_authentication/api/v1/auth/session_auth.py
@@ -19,6 +19,6 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Returns a User ID based on a Session ID"""
-         if session_id is None or type(session_id) is not str:
-             return None
-         return self.user_id_by_session_id.get(session_id)
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
