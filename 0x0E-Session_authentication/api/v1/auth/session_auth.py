1d728995bb6130428502ed29c70c002e3c5c71b2
Criptograma-1 <andresa.info@gmail.com>
2022-07-13 14:46:42 +0000

    [ADD] 2. Create a session

--- 0x0E-Session_authentication/api/v1/auth/session_auth.py
+++ 0x0E-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,17 @@
 """Class that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """Sessioon Authentication"""
-    user_id = None
+    """Session Authentication"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
