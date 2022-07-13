c633f94bbcf620d7e9b7e0c54ceee93c3b3f2689
Criptograma-1 <andresa.info@gmail.com>
2022-07-13 15:19:12 +0000

    [ADD] 6. Use Session ID for identifying a User

--- 0x0E-Session_authentication/api/v1/auth/session_auth.py
+++ 0x0E-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +23,9 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
