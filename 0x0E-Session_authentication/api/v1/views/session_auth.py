ea85be30baa6b9651975c2450f4f15a3b2e4ac40
Criptograma-1 <andresa.info@gmail.com>
2022-07-13 15:35:53 +0000

    [ADD] 7. New view for Session Authentication

---
+++ 0x0E-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" View that handles all routes for the Session authentication
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+    """Route POST /auth_session/login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+    try:
+        users = User.search({'email': user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_cookie = getenv("SESSION_NAME")
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(session_cookie, session_id)
+    return response
