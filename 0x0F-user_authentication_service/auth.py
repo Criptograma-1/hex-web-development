874a016549b7e3da58091d2a249b88808e63c8a3
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-15 14:16:14 -0300

    8. Credentials validation

--- 0x0F-user_authentication_service/auth.py
+++ 0x0F-user_authentication_service/auth.py
@@ -33,3 +33,11 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Valid login"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return checkpw(password.encode('utf-8'), user.hash_password)
