4ce5d65424057efd82a704627171cd620250a5a8
Criptograma-1 <andresa.info@gmail.com>
2022-07-15 17:05:02 +0000

    [ADD] 5. Register user

--- 0x0F-user_authentication_service/auth.py
+++ 0x0F-user_authentication_service/auth.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
 """ Auth script """
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """Returns a hashed password"""
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register user
+
+        Args:
+            email (string): user email
+            password (string): user password
+
+        Returns:
+            User registered
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
