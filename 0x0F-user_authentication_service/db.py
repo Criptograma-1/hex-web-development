6afbff9fbef3418adfff7d1fc367bbbacb833cd5
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-15 12:41:05 -0300

    Update db.py

--- 0x0F-user_authentication_service/db.py
+++ 0x0F-user_authentication_service/db.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+"""
+BD class
+"""
+
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
+
 
 DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 
@@ -13,7 +18,6 @@ DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 class DB:
 
     def __init__(self):
-        """init mathod"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -21,7 +25,6 @@ class DB:
 
     @property
     def _session(self):
-        """Session method"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -29,9 +32,11 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user to database
-            - email (string): email of user
-            - hashed_password (string): password of user
-        Returns user created
+        Args:
+            email (string): email of user
+            hashed_password (string): password of user
+        Returns:
+            User: user created
         """
         if not email or not hashed_password:
             return
@@ -42,25 +47,24 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find user, method that takes as argument:
-            - arbitrary keyword arguments
-        Returns he first row found in the users table as
-         filtered by the method’s input arguments
+        """find user by some arguments
+        Returns:
+            User: user found or raise error
         """
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
             raise NoResultFound
         return user
-        
-     def update_user(self, user_id: int, **kwargs):
-         """Update user
-         Args:
-             user_id (int): id of user
-         """
-         user = self.find_user_by(id=user_id)
-         for key, val in kwargs.items():
-             if key not in DATA:
-                 raise ValueError
-             setattr(user, key, val)
-         self._session.commit()
-         return None
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user
+        Args:
+            user_id (int): id of user
+        """
+        user = self.find_user_by(id=user_id)
+        for key, val in kwargs.items():
+            if key not in DATA:
+                raise ValueError
+            setattr(user, key, val)
+        self._session.commit()
+        return None
