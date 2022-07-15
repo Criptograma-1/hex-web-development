ed1b53f424119c4c1fd3eaaee69d6718215cbfc1
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-15 14:13:33 -0300

    7. Register user

--- 0x0F-user_authentication_service/app.py
+++ 0x0F-user_authentication_service/app.py
@@ -2,8 +2,11 @@
 """
 Flask app
 """
-from flask import Flask, jsonify,
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -11,3 +14,15 @@ app = Flask(__name__)
 def index() -> str:
     """Index route"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user() -> str:
+    """ Route for user register (POST)"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"messege": "email already registered"}), 400
