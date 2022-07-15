f668bea7d29036ce944d8ca13503cfa58b290b1a
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-15 15:02:32 -0300

    Update auth.py

--- 0x0F-user_authentication_service/auth.py
+++ 0x0F-user_authentication_service/auth.py
@@ -77,6 +77,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return
 
